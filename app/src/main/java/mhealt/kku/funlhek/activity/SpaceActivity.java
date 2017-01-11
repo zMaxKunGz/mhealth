ff933c83644648112b29b2e77e72d0ad277e238f
vashiravit.m <vashiravit.m@gmail.com>
2017-01-11 20:45:08 +0700

    Add Firebase

---
+++ app/src/main/java/mhealt/kku/funlhek/activity/SpaceActivity.java
@@ -0,0 +1,26 @@
+package mhealt.kku.funlhek.activity;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import mhealt.kku.funlhek.R;
+
+public class SpaceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_space);
+        // Auto Intend
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SpaceActivity.this, MainActivity.class));
+                finish();
+            }
+        }, 2000); // หน่วงเวลา
+    }
+}

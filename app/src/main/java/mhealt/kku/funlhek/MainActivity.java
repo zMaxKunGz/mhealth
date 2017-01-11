5e07ca3fff2aa1d870b22387463734dad92d81d4
vashiravit.m <vashiravit.m@gmail.com>
2017-01-11 14:59:11 +0700

    start init

---
+++ app/src/main/java/mhealt/kku/funlhek/MainActivity.java
@@ -0,0 +1,13 @@
+package mhealt.kku.funlhek;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
